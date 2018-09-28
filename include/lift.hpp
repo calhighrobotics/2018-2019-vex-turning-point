01a935790bfbe55213eaa15eaf3f84666e665366
Vihan <vihan+github@vihan.org>
2018-09-27 20:22:53 -0700

    Add beautiful code for event loop
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- include/lift.hpp
+++ include/lift.hpp
@@ -10,6 +10,17 @@ namespace lift
 void initEncoders();
 
 /**
+ * Initializes the event loop to adjust motors
+ */
+void initEventLoop();
+
+/**
+ * Sets lift position
+ * @param position A value from 0 to 1 specifying relative extension of lift
+ */
+void setLiftPosition(float position);
+
+/**
  * Sets the lift motors.
  * @param power Motor power.
  */
--- src/lift.cpp
+++ src/lift.cpp
@@ -2,14 +2,28 @@
 #include "main.hpp"
 #include "motor.hpp"
 #include "sensor.hpp"
+#include <cmath>
 
 using namespace motor;
 using namespace sensor;
 
 /** Left lift encoder. */
 static Encoder leftEncoder = nullptr;
+
 /** Right lift encoder. */
 static Encoder rightEncoder = nullptr;
+
+/** The event loop object. */
+static TaskHandle eventLoopTask = nullptr;
+
+/** The desired position of the lift. */
+static float targetPosition = 0;
+
+/** How many encoder values between retracted and fully extended. */
+static constexpr int ticksForExtension = 360;
+
+/** Length of a tick of encoder */
+static constexpr int tickTime = 15;
 
 void lift::initEncoders()
 {
@@ -17,6 +31,40 @@ void lift::initEncoders()
         /*reverse=*/ true);
     rightEncoder = encoderInit(LIFT_RIGHT_TOP, LIFT_RIGHT_BOTTOM,
         /*reverse=*/ false);
+}
+
+static void eventLoopTick(void*)
+{
+    unsigned long lastTaskRunTime = millis();
+
+    while (true)
+    {
+        // Do happy things
+        float targetLiftPosition = targetPosition;
+        int targetEncoderValue = ticksForExtension * targetLiftPosition;
+
+        // TODO: handle syncing of multiple encoders
+        int currentEncoderValue = encoderGet(rightEncoder);
+
+        // Determine which way we should go
+        int encoderDelta = targetEncoderValue - currentEncoderValue;
+
+        // Returns power (clamped to [-127, 127]) given encoder delta.
+        int power = round(127 * tanh((1.0f / ticksForExtension) * encoderDelta));
+        lift::set(power);
+
+        taskDelayUntil(&lastTaskRunTime, tickTime);
+    }
+}
+
+void lift::initEventLoop()
+{
+    eventLoopTask = taskCreate(eventLoopTick, TASK_DEFAULT_STACK_SIZE, nullptr, TASK_PRIORITY_DEFAULT);
+}
+
+void lift::setLiftPosition(float position)
+{
+    targetPosition = position;
 }
 
 void lift::set(int power)
