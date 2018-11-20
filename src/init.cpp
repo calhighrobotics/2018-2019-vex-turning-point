f5fd00f4e3a1d6cf9eb28095ff6cef4cf550a6cd
CrazyGuy108 <taylor@hansenfamily.us>
2018-11-20 15:53:41 -0800

    Fix linker error

--- src/init.cpp
+++ src/init.cpp
@@ -2,6 +2,9 @@
 #include "lift.hpp"
 #include "puncher.hpp"
 
+// not having this causes an undefined reference error for now
+void* __dso_handle;
+
 void initializeIO()
 {
     puncher::initLimit();
