72d23442941c9d43fece038412b0956431db18d3
CrazyGuy108 <taylor@hansenfamily.us>
2019-01-26 19:55:43 -0800

    Allow PID::init() to be called more than once
    May be needed to adjust PID parameters midway.

--- src/pid.cpp
+++ src/pid.cpp
@@ -5,12 +5,24 @@
 
 void PID::init(float kP, float kI, float kD, int minOut, int maxOut)
 {
-    this->kP = kP;
-    this->kI = kI;
-    this->kD = kD;
+    if (mutex) mutexTake(mutex, 0);
+
+    // initialize or update fields
     this->minOut = minOut;
     this->maxOut = maxOut;
-    mutex = mutexCreate();
+    // don't reset targetPos because that's handled by another function
+    this->kP = kP;
+    this->kI = kI;
+    // don't reset integral because we don't want to have to re-integrate the
+    //  offset, and will reclamp itself on the next update() call
+    this->kD = kD;
+    for (float& deriv : derivs) deriv = 0;
+    // don't reset oldestDeriv because its index within derivs doesn't matter
+    //  anymore
+    // don't reset lastError because that would cause a derivative spike
+
+    if (mutex) mutexGive(mutex);
+    else mutex = mutexCreate();
 }
 
 void PID::setTargetPos(int pos)
@@ -18,10 +30,8 @@ void PID::setTargetPos(int pos)
     mutexTake(mutex, 0);
     if (pos != targetPos)
     {
-        // new target position means previous states have to be reset
-        integral = 0;
+        // new target position means previous (unscaled) derivs have to be reset
         for (float& deriv : derivs) deriv = 0;
-        lastError = 0;
     }
     targetPos = pos;
     mutexGive(mutex);
@@ -39,7 +49,8 @@ int PID::update(int value, int deltaTime)
     // integral term
     integral += kI * error * deltaTime;
     // clamp integral between minOut/maxOut to prevent windup
-    integral = std::max((float) minOut, std::min(integral, (float) maxOut));
+    integral = std::max(static_cast<float>(minOut),
+            std::min(integral, static_cast<float>(maxOut)));
     const float i = integral;
 
     // derivative term
@@ -57,6 +68,7 @@ int PID::update(int value, int deltaTime)
     const int power = std::max(minOut, std::min(pid, maxOut));
     printf("enc=%d target=%d p=%.2f i=%2.f d=%.2f power=%d\n", value, targetPos,
         p, i, d, power);
+
     mutexGive(mutex);
     return power;
 }
